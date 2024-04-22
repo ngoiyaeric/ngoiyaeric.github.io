4eb105621b0db7114c88748601410741ff4926b4
Eric <info.ericmail@gmail.com>
2024-04-22 19:46:08 +0300

    Update MyLinks.tsx

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { AiFillGithub, AiFillLinkedin, AiOutlineMediumWorkmark, AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiOutlineMediumWorkmark, AiFillTwitterCircle, AiFillInstagram} from "react-icons/ai";
 import styles from "./headinglinks.module.scss";
 
 export const MyLinks = () => {
@@ -32,7 +32,19 @@ export const MyLinks = () => {
           <AiFillLinkedin size="2.4rem" />
         </Link>
       </motion.span>
-
+       <motion.span
+        initial={{ y: -30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+      >
+        <Link
+          href="https://instagram/eq.waves"
+          target="_blank"
+          rel="nofollow"
+        >
+          <AiFillInstagram size="2.4rem" />
+        </Link>
+      </motion.span>
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
