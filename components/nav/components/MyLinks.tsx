d8e93aa762683f4f6a368e96ab5f4f975202fba9
Eric <info.ericmail@gmail.com>
2024-04-22 19:25:06 +0300

    Update MyLinks.tsx

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -1,11 +1,24 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { AiFillGithub, AiFillLinkedin, AiOutlineMediumWorkmark } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiOutlineMediumWorkmark, AiFillTwitterCircle } from "react-icons/ai";
 import styles from "./headinglinks.module.scss";
 
 export const MyLinks = () => {
   return (
     <div className={styles.links}>
+       <motion.span
+        initial={{ y: -30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.0 }}
+      >
+        <Link
+          href="https://www.X.com/ereqlive"
+          target="_blank"
+          rel="nofollow"
+        >
+          <AiFillTwitterCircle size="2.4rem" />
+        </Link>
+      </motion.span>
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
