8ad47172915cea766afa49d468a7f9c4d6b1be99
EreQ <info.ericmail@gmail.com>
2024-05-24 12:20:34 +0300

    Update MyLinks.tsx

--- components/nav/components/MyLinks.tsx
+++ components/nav/components/MyLinks.tsx
@@ -6,19 +6,6 @@ import styles from "./headinglinks.module.scss";
 export const MyLinks = () => {
   return (
     <div className={styles.links}>
-       <motion.span
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.0 }}
-      >
-        <Link
-          href="https://www.X.com/ereqlive"
-          target="_blank"
-          rel="nofollow"
-        >
-          <AiFillTwitterCircle size="2.4rem" />
-        </Link>
-      </motion.span>
       <motion.span
         initial={{ y: -30, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
@@ -30,19 +17,6 @@ export const MyLinks = () => {
           rel="nofollow"
         >
           <AiFillLinkedin size="2.4rem" />
-        </Link>
-      </motion.span>
-       <motion.span
-        initial={{ y: -30, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
-        <Link
-          href="https://instagram/eq.waves"
-          target="_blank"
-          rel="nofollow"
-        >
-          <AiFillInstagram size="2.4rem" />
         </Link>
       </motion.span>
       <motion.span
