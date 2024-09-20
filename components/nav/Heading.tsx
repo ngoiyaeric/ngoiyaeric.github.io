85f2138ed19effe4cde2afe9e9a96ab917d19319
EreQ <info.ericmail@gmail.com>
2024-09-20 14:10:44 +0300

    Update Heading.tsx

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -7,7 +7,7 @@ export const Heading = () => {
     <header className={styles.heading}>
       <MyLinks />
       <OutlineButton
-        onClick={() => window.open("https://drive.google.com/file/d/13bC2TbZm26201Uzb_HYtNNUkI9uA1GZ3/view?usp=sharing")}
+        onClick={() => window.open("https://drive.google.com/file/d/1tXgS8GVly84HNoR1QB42SP5lTJAL8x-C/view?usp=sharing")}
       >
         Resume
       </OutlineButton>
